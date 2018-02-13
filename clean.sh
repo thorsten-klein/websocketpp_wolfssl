3859d770a5e427518351dc0eb47ada23a826e4ae
Klein, Thorsten (BSH) <thorsten.klein@bshg.com>
2018-02-13 08:33:02 +0100

    Update

--- clean.sh
+++ clean.sh
@@ -2,7 +2,15 @@
 
 if [ $? -ne 0 ]; then /bin/bash -c "$0"; exit; fi
 
+# clean this repository
+git clean -d -f
+git reset --hard
 rm -rf ./wolfssl/wolfssl-3.13.0
+
+# clean submodule websocketpp
+cd websocketpp
+git clean -d -f
+git reset --hard
 
 echo
 echo "----------------------------------------------------"
