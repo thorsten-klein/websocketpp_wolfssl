69742a624f316af60758a02779ff634a5149bc00
Klein, Thorsten (BSH) <thorsten.klein@bshg.com>
2018-02-13 08:38:51 +0100

    Update

--- build.sh
+++ build.sh
@@ -22,22 +22,26 @@ make install DESTDIR=$WOLFSSL_INSTALL_DIR
 
 
 # compile websockettpp example
-cd $ROOT_DIR/websocketpp/examples/debug_client
+EXAMPLE_DIR=$ROOT_DIR/websocketpp/examples/debug_client
 
-#normally
+# using openssl
+cd $EXAMPLE_DIR
 mkdir with_openssl
-g++ ../*.cpp -I$WEBSOCKETPP_DIR -I$ASIO_DIR/include -std=c++11 -I$WOLFSSL_SRC_DIR/wolfssl -lcrypto -lboost_system -lpthread -lssl
+cd with_openssl
+echo "Compiling $PWD ..."
+g++ ../*.cpp -std=c++11 -I$WEBSOCKETPP_DIR -I$ASIO_DIR/include -lcrypto -lboost_system -lpthread -lssl
 
-cd..
-
-#with wolfssl
+# using wolfssl
+cd $EXAMPLE_DIR
 mkdir with_wolfssl
-g++ ../*.cpp -I$WEBSOCKETPP_DIR -I$ASIO_DIR/include -std=c++11 -I$WOLFSSL_SRC_DIR/wolfssl -lcrypto -lboost_system -lpthread -I$WOLFSSL_INSTALL_DIR/usr/local/include -I$WOLFSSL_INSTALL_DIR/usr/local/include/wolfssl
+cd with_wolfssl
+echo "Compiling $PWD ..."
+g++ ../*.cpp -std=c++11 -I$WEBSOCKETPP_DIR -I$ASIO_DIR/include -I$WOLFSSL_SRC_DIR/wolfssl -lcrypto -lboost_system -lpthread -I$WOLFSSL_INSTALL_DIR/usr/local/include -I$WOLFSSL_INSTALL_DIR/usr/local/include/wolfssl
 
 
-# using ASIO Standalone
+# NOTE: using ASIO Standalone
 ## DOES NOT WORK WITH OPENSSL
-## OPENSSL NEEDS BOOST ASIO
+## --> OPENSSL NEEDS BOOST ASIO
 #
 # g++ ../*.cpp -I$WEBSOCKETPP_DIR -I$ASIO_DIR/include -std=c++11 -I$WOLFSSL_SRC_DIR/wolfssl -lcrypto -lboost_system -lpthread -lssl -DASIO_STANDALONE
 
