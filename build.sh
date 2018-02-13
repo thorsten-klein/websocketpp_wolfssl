c8b460568522a518c514e7848d675b83cd0e803a
Klein, Thorsten (BSH) <thorsten.klein@bshg.com>
2018-02-13 08:31:35 +0100

    Update

--- build.sh
+++ build.sh
@@ -3,10 +3,15 @@
 if [ $? -ne 0 ]; then /bin/bash -c "$0"; exit; fi
 
 ROOT_DIR=$PWD
+
+WEBSOCKETPP_DIR=$ROOT_DIR/websocketpp
+ASIO_DIR=$ROOT_DIR/asio/asio-1.10.8
 WOLFSSL_DIR=$ROOT_DIR/wolfssl
+
 WOLFSSL_SRC_ZIP=$WOLFSSL_DIR/wolfssl-3.13.0.zip
 WOLFSSL_SRC_DIR=$WOLFSSL_DIR/wolfssl-3.13.0
 WOLFSSL_INSTALL_DIR=$WOLFSSL_SRC_DIR/installed
+
 
 
 # compile wolfssl
@@ -15,21 +20,27 @@ cd $WOLFSSL_SRC_DIR
 ./configure --enable-opensslextra
 make install DESTDIR=$WOLFSSL_INSTALL_DIR
 
-exit
 
 # compile websockettpp example
 cd $ROOT_DIR/websocketpp/examples/debug_client
-g++ *.cpp -I../.. -I$ROOT_DIR/asio/asio-1.10.8/include -DASIO_STANDALONE  -std=c++11 -lcrypto -I$ROOT_DIR/wolfssl/wolfssl-3.13.0/wolfssl
-g++ *.cpp -I../.. -I$ROOT_DIR/asio/asio-1.10.8/include -DASIO_STANDALONE  -std=c++11 -I$WOLFSSL_SRC_DIR/wolfssl 
+
+#normally
+mkdir with_openssl
+g++ ../*.cpp -I$WEBSOCKETPP_DIR -I$ASIO_DIR/include -std=c++11 -I$WOLFSSL_SRC_DIR/wolfssl -lcrypto -lboost_system -lpthread -lssl
+
+cd..
+
+#with wolfssl
+mkdir with_wolfssl
+g++ ../*.cpp -I$WEBSOCKETPP_DIR -I$ASIO_DIR/include -std=c++11 -I$WOLFSSL_SRC_DIR/wolfssl -lcrypto -lboost_system -lpthread -I$WOLFSSL_INSTALL_DIR/usr/local/include -I$WOLFSSL_INSTALL_DIR/usr/local/include/wolfssl
 
 
-make
-make install DESTDIR=$ROOT_DIR/wolfssl/wolfssl-3.13.0/install
-g++ -std=c++11 -I$ROOT_DIR/wolfssl/wolfssl-3.13.0 -I$ROOT_DIR/asio/asio-1.10.8/include -I/home/default/GIT/websocketpp/ *.cpp -lboost_system -lcrypto -l$ROOT_DIR/wolfssl/wolfssl-3.13.0/install/usr/local/lib/libwolfssl.so
-g++ *.cpp -I../.. -I$ROOT_DIR/asio/asio-1.10.8/include -DASIO_STANDALONE  -std=c++11 -I$ROOT_DIR/wolfssl/wolfssl-3.13.0/wolfssl 
-pkg-config --cflags wolfssl -DBOOST_NETWORK_ENABLE_HTTPS
-g++ *.cpp -I../.. -I$ROOT_DIR/asio/asio-1.10.8/include -DASIO_STANDALONE  -std=c++11 -I$ROOT_DIR/wolfssl/wolfssl-3.13.0/wolfssl -DBOOST_NETWORK_ENABLE_HTTPS
-g++ *.cpp -I../.. -I$ROOT_DIR/asio/asio-1.10.8/include -DASIO_STANDALONE  -std=c++11 -I$ROOT_DIR/wolfssl/wolfssl-3.13.0/wolfssl -lboost_system
+# using ASIO Standalone
+## DOES NOT WORK WITH OPENSSL
+## OPENSSL NEEDS BOOST ASIO
+#
+# g++ ../*.cpp -I$WEBSOCKETPP_DIR -I$ASIO_DIR/include -std=c++11 -I$WOLFSSL_SRC_DIR/wolfssl -lcrypto -lboost_system -lpthread -lssl -DASIO_STANDALONE
+
 
 
 
